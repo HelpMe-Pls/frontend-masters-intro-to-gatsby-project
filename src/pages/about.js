e7dc8f57403ba714b4dfba692dc89e353084a929
Random Guy <leekhoi5319990@gmail.com>
2021-11-14 11:07:01 +0700

    .

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import * as React from 'react'; //must have
+import { Link } from 'gatsby';
 
+// every components MUST be <default> export
 export default function AboutPage() {
-	// every components MUST be <default> export
 	return (
 		<main>
 			<h1>Look MAAA !</h1>
+			<Link to="/">Back to home </Link>
+			{/* Using <Link> actually makes it faster than using the back button, without having to refresh the page */}
 		</main>
 	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import * as React from 'react'; //must have
+import { Link } from 'gatsby';
 
+// every components MUST be <default> export
 export default function IndexPage() {
-	// every components MUST be <default> export
 	return (
 		<main>
 			<h1>Look MA !</h1>
+			<Link to="/about">About</Link>
+			{/* Using <Link> actually makes it faster than using the back button, without having to refresh the page */}
 		</main>
 	);
 }
