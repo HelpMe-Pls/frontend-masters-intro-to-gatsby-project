5f00fb62459244ad23c15f3c0b80873c667a4068
Random Guy <leekhoi5319990@gmail.com>
2021-11-14 12:16:00 +0700

    Metadata & configs

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	siteMetadata: {
+		siteUrl: 'https://www.randomguy.com',
+		title: 'Randomguy',
+		description:
+			'Randomguy is a random guy who is a web developer and a writer.',
+		image: 'https://www.stjude.cloud/static/data-universe-v500-2fd94de5b7fee27353e38838e2814503.png'
+	}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,30 @@
 import * as React from 'react'; //must have
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 // every components MUST be <default> export
 export default function IndexPage() {
+	const data = useStaticQuery(graphql`
+		query GetSiteTitle {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}
+	`);
+
+	const meta = data?.site?.siteMetadata ?? {};
+
 	return (
-		<main>
-			<h1>Look MA !</h1>
-			<Link to="/about">About</Link>
-			{/* Using <Link> actually makes it faster than using the back button, without having to refresh the page */}
-		</main>
+		<>
+			<header>
+				<Link to="/">{meta.title}</Link>
+			</header>
+			<main>
+				<h1>Look MA !</h1>
+				<Link to="/about">About</Link>
+				{/* Using <Link> actually makes it faster than using the back button, without having to refresh the page */}
+			</main>
+		</>
 	);
 }
