cc3f7ace2c6e85ca44e239d86c81450d30d4a8d4
Random Guy <leekhoi5319990@gmail.com>
2021-11-15 12:42:11 +0700

    CSS Module

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,13 +2,14 @@ import * as React from 'react'; //must have
 import Seo from '../components/seo.js';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import '../styles/global.css';
+import { header, content } from '../styles/layout.module.css';
 
 const Layout = ({
 	children,
 	title = false,
 	description = false,
 	image = false,
-	path = false
+	path = false,
 }) => {
 	const data = useStaticQuery(graphql`
 		query GetSiteTitle {
@@ -29,13 +30,13 @@ const Layout = ({
 				image={image}
 				path={path}
 			/>
-			<header>
+			<header className={header}>
 				<Link to="/">{meta.title}</Link>
 				<nav>
 					<Link to="/about">About</Link>
 				</nav>
 			</header>
-			<main>{children}</main>
+			<main className={content}>{children}</main>
 		</>
 	);
 };
