022ad5e5ae115ff537f0077defea965155366483
Random Guy <leekhoi5319990@gmail.com>
2021-11-15 23:27:50 +0700

    MDX && Frontmatter

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,32 @@ module.exports = {
 		title: 'Randomguy',
 		description:
 			'Randomguy is a random guy who is a web developer and a writer.',
-		image: 'https://www.stjude.cloud/static/data-universe-v500-2fd94de5b7fee27353e38838e2814503.png'
+		image: 'https://www.stjude.cloud/static/data-universe-v500-2fd94de5b7fee27353e38838e2814503.png',
 	},
-	plugins: ['gatsby-plugin-react-helmet']
+	plugins: [
+		'gatsby-plugin-react-helmet',
+
+		// These are the plugins that are used to enable the MDX rules for pages from src/posts
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `posts`,
+				path: `${__dirname}/src/posts`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-page-creator`,
+			options: {
+				path: `${__dirname}/src/posts`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-mdx`,
+			options: {
+				defaultLayouts: {
+					posts: require.resolve('./src/components/layout.js'),
+				},
+			},
+		},
+	],
 };
